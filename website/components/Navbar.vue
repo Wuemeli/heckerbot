3d5d902ee380d0b32e974af16302ea712e245a73
Wuemeli <wuemeli@gmail.com>
2023-11-30 19:37:39 +0100

    Website Update

--- website/components/Navbar.vue
+++ website/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
       <div class="navbar navbar-expand-md bg-dark navbar-dark">
           <div class="container">
-              <a href="" class="navbar-brand text-info">Aris Bot</a>
+              <a href="" class="navbar-brand text-info">Heckerbot</a>
 
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu"><span
                       class="navbar-toggler-icon"></span></button>
@@ -9,16 +9,9 @@
               <div class="collapse navbar-collapse" id="mainmenu">
                   <ul class="navbar-nav mx-auto">
                       <li class="nav-item"><a href="#Features" class="nav-link">Features</a></li>
-                      <li class="nav-item"><a href="#Statics" class="nav-link">Statics</a></li>
-                      <li class="nav-item"><a href="#FAQ" class="nav-link">Ask me</a></li>
+                      <li class="nav-item"><a href="#Stats" class="nav-link">Stats</a></li>
+                      <li class="nav-item"><a href="#FAQ" class="nav-link">FAQ</a></li>
                       <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" href="#" id="linksdrop" role="button"
-                              data-bs-toggle="dropdown" aria-expanded="false">Links</a>
-                          <ul class="dropdown-menu bg-dark" aria-labelledby="linksdrop">
-                              <li><a class="dropdown-item text-muted" href="">Invite</a></li>
-                              <li><a class="dropdown-item text-muted" href="">Github</a></li>
-                              <li><a class="dropdown-item text-muted" href="">Support Server</a></li>
-                          </ul>
                       </li>
                   </ul>
                   <li class="navbar-nav nav-item"><a href="" class="btn btn-outline-info">Login</a></li>
--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'Wuemeli\'s Portfolio 🔥',
+      title: 'Heckerbot 🔥',
       htmlAttrs: {
         lang: 'en'
       },
