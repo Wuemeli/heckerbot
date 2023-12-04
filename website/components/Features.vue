239d81dfa9ea119e84385247d9ed181a9df1e991
Wuemeli <wuemeli@gmail.com>
2023-12-04 12:37:17 +0100

    Transfers the Code to Tailwindcss (THIS WAS HARD WORK)

--- website/components/Features.vue
+++ website/components/Features.vue
@@ -12,8 +12,8 @@
                           </div>
                           <div class="card-body text-center">
                               <h4 class="card-title text-info">Easy to Use</h4>
-                              <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
-                                  illo nihil nesciunt cum sequi rem.</p>
+                              <p class="card-text">We tried our best to make it as easy as possible to use. Everything
+                                  is well documented and divided into easy to understand sections.</p>
                           </div>
                       </div>
                   </div>
@@ -23,9 +23,9 @@
                               <i class="bi bi-capslock-fill"></i>
                           </div>
                           <div class="card-body text-center">
-                              <h4 class="card-title text-info">24/7 Online</h4>
-                              <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
-                                  illo nihil nesciunt cum sequi rem.</p>
+                              <h4 class="card-title text-info">Open Source</h4>
+                              <p class="card-text">Our Bot is Open Source, so you can contribute to it and make it
+                                  better. You can also host it yourself if you want.</p>
                           </div>
                       </div>
                   </div>
@@ -35,9 +35,9 @@
                               <i class="bi bi-folder2-open"></i>
                           </div>
                           <div class="card-body text-center">
-                              <h4 class="card-title text-info">Open Source</h4>
-                              <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
-                                  illo nihil nesciunt cum sequi rem.</p>
+                              <h4 class="card-title text-info">NON-PROFIT</h4>
+                              <p class="card-text">We are a Non-Profit "Organization", so we don't make any money from
+                                  this. We do this because we love to do it.</p>
                           </div>
                       </div>
                   </div>
--- website/components/Info.vue
+++ website/components/Info.vue
@@ -4,7 +4,7 @@
       <div class="d-flex justify-content-center align-items-center">
         <div>
           <h1><span class="text-info">Hecker</span> Bot</h1>
-          <p>Heckerbot is a multipurpose discord bot with a lot of features.</p>
+          <p>A Opensource Discord Bot that brings Fun and Security to your Server</p>
           <a class="btn btn-info" href="">Invite Bot</a>
           <a class="m-2 btn btn-light" href="">Support Server</a>
         </div>
