07467dda54220f6e1770bc1a74cff14ac46c9c5f
Wuemeli <wuemeli@gmail.com>
2023-12-20 10:25:42 +0100

    Removal of the Not Loading to the API. Will be made soon

--- src/handlers/deploy.js
+++ src/handlers/deploy.js
@@ -6,22 +6,17 @@ const {log} = require('../functions');
  * @param {ExtendedClient} client
  */
 module.exports = async (client, token, clientId) => {
-  const rest = new REST({ version: '10' }).setToken(token);
+  const rest = new REST({version: '10'}).setToken(token);
 
   try {
     log('Started loading application commands... (this might take minutes!)', 'warn');
 
-    const enabledCommands = client.applicationcommandsArray.filter(
-      command => command.options && command.options.category && process.env[command.options.category.toUpperCase()] !== 'false',
-    );
-
     await rest.put(Routes.applicationCommands(clientId), {
-      body: enabledCommands,
+      body: client.applicationcommandsArray,
     });
 
     log('Successfully loaded application commands to Discord API.', 'done');
   } catch (e) {
     log('Unable to load application commands to Discord API.', 'err');
-    log(e, 'err'); // Log the error message
   }
 };
