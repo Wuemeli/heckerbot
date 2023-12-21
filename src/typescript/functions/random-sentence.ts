8c48f7d27217a6f51464fa0728de939db9248bdb
Wuemeli <wuemeli@gmail.com>
2023-12-21 20:22:30 +0100

    Remove the `random-sentence` function from `fasttype` command and replaced it with a Custom Coded Sentence Generator.

---
+++ src/typescript/functions/random-sentence.ts
@@ -0,0 +1,26 @@
+export async function random_sentence() {
+  const wordArray = [
+    'apple', 'banana', 'cherry', 'dog', 'elephant', 'flower', 'guitar', 'happy', 'island', 'jazz',
+    'kangaroo', 'lemon', 'mountain', 'notebook', 'ocean', 'piano', 'quasar', 'rabbit', 'sunset', 'tiger',
+    'umbrella', 'victory', 'waterfall', 'xylophone', 'yellow', 'zeppelin', 'acoustic', 'butterfly', 'captain', 'delicious',
+    'elephant', 'fantastic', 'giraffe', 'happiness', 'illusion', 'jubilant', 'kaleidoscope', 'lighthouse', 'magnificent', 'nostalgia',
+    'orchestra', 'paradise', 'quixotic', 'rhapsody', 'serendipity', 'tranquility', 'ultimate', 'velvet', 'whisper', 'xenial',
+    'yesterday', 'zephyr', 'alchemy', 'bewilder', 'cathedral', 'dandelion', 'ethereal', 'fandango', 'gossamer', 'harmony',
+    'incandescent', 'juxtapose', 'kaleidoscope', 'labyrinth', 'melancholy', 'nirvana', 'oblivion', 'paradox', 'quasar', 'reverie',
+    'serendipity', 'talisman', 'umbrella', 'vivid', 'whimsical', 'xanadu', 'yearning', 'zenith', 'aplomb', 'blissful',
+    'cacophony', 'dexterity', 'effervescent', 'felicity', 'gallivant', 'halcyon', 'impeccable', 'jubilant', 'kismet', 'luminescent',
+    'mellifluous', 'nurturing', 'opulent', 'quintessential', 'resplendent', 'serene', 'taciturn', 'ubiquitous', 'vestige', 'whimsical',
+    'xenial', 'yearning', 'zephyr'
+  ];
+
+  const sentenceLength = Math.floor(Math.random() * (12 - 6 + 1)) + 6;
+  let randomSentence = '';
+
+  for (let i = 0; i < sentenceLength; i++) {
+    const randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    randomSentence += randomWord + ' ';
+  }
+
+  console.log(randomSentence);
+  return randomSentence.trim();
+}
