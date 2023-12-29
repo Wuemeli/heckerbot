9a7d3f829cce0ee4a2a16adcad130249910549f5
Wuemeli <wuemeli@gmail.com>
2023-12-29 12:52:14 +0100

    Fix reminder

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -3,18 +3,6 @@ const ExtendedClient = require('../../class/ExtendedClient');
 const { botInfo } = require('../../functions/custom-bot/main');
 const { checkReminders } = require('../../functions/functions/interval');
 
-async function setUserActivity(client, totalUsers, guildcount) {
-  const check = await botInfo(client.user.id);
-  let status;
-  if (check) {
-    status = check.status.replace('{users}', totalUsers).replace('{guilds}', guildcount);
-  } else {
-    status = `${guildcount} servers | ${totalUsers} users | Made with ❤️ by Wuemeli`;
-  }
-  client.user.setActivity(status, { type: 4 });
-  return check;
-}
-
 module.exports = {
   event: 'ready',
   once: true,
@@ -25,16 +13,32 @@ module.exports = {
      * @returns
   */
   run: async (_, client) => {
-    let totalUsers = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
+    const totalUsers = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
     const guildcount = client.guilds.cache.size;
 
-    await setUserActivity(client, totalUsers, guildcount);
-    checkReminders(client);
-    log(`Logged in as ${client.user.tag}`, 'info');
+    const check = await botInfo(client.user.id);
+    let status;
+    if (check) {
+      status = check.status.replace('{users}', totalUsers).replace('{guilds}', guildcount);
+      checkReminders(client);
+    } else {
+      status = `${guildcount} servers | ${totalUsers} users | Made with ❤️ by Wuemeli`;
+      checkReminders(client);
+      log(`Logged in as ${client.user.tag}`, 'info');
+    }
+    client.user.setActivity(status, { type: 4 });
 
     setInterval(async () => {
-      totalUsers = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
-      await setUserActivity(client, totalUsers, guildcount);
+      const check = await botInfo(client.user.id);
+      let status;
+      if (check) {
+        status = check.status.replace('{users}', totalUsers).replace('{guilds}', guildcount);
+        checkReminders(client);
+      } else {
+        status = `${guildcount} servers | ${totalUsers} users | Made with ❤️ by Wuemeli`;
+        checkReminders(client);
+      }
+      client.user.setActivity(status, { type: 4 });
     }, 60000);
   },
 };
