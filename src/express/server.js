7b70eff472f793761c87f2f0bcfeb9649b128331
Wuemeli <wuemeli@gmail.com>
2024-01-23 09:34:37 +0100

    Makes that 30 Servers get outputed

--- src/express/server.js
+++ src/express/server.js
@@ -57,7 +57,7 @@ module.exports = {
 
       const topGuilds = Array.from(client.guilds.cache.values())
         .sort((a, b) => b.memberCount - a.memberCount)
-        .slice(0, 10);
+        .slice(0, 30);
 
       const topggvotes = await getBotVotes(client);
 
