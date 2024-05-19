a59707d791e2956ee7edcf96a6ba32162768ca71
Wuemeli <wuemeli@gmail.com>
2024-05-19 16:10:02 +0200

    Removes unused Code

--- src/functions/functions/interval.ts
+++ src/functions/functions/interval.ts
@@ -3,7 +3,6 @@ import { EmbedBuilder } from 'discord.js';
 import backupSchema from '../../schemas/backupSchema';
 import backup from '../backup/index';
 import guildSettings from '../../schemas/guildSchema';
-import sendtodev from './sendtodev';
 
 export async function checkReminders(client: any) {
   const reminders = await reminderSchema.find({ time: { $lt: new Date() } });
