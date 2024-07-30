ec76f61a2bc095a246e618122e66d7659bc23ee7
Wuemeli <wuemeli@gmail.com>
2024-07-30 08:45:25 +0200

    Discord ratelimit fixed soon

--- src/functions/functions/cron.ts
+++ src/functions/functions/cron.ts
@@ -1,9 +1,11 @@
 import cron from 'node-cron';
 const { checkExpiredPremium } = require('../custom-bot/premium');
 
+/*
 export function scheduleJobs(client: any): void {
   checkExpiredPremium().catch(console.error);
   cron.schedule('0 0 * * *', () => {
     checkExpiredPremium().catch(console.error);
   });
 }
+*/
